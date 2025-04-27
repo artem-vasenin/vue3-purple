e0d7c706390ced1c2e3f254df899bb95a6599471
Rusich <vasenin@verqor.com>
2025-04-27 13:46:43 +0300

    added btn component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <header>Header</header>
-  <main>main</main>
+  <main>
+    <Btn>
+      <template #iconPrefix>
+        <img src="./assets/map-marker.svg" alt="icon"/>
+      </template>
+      Изменить город
+    </Btn>
+  </main>
   <footer>Footer</footer>
 </template>
 
 <script setup>
-
+import Btn from './components/Btn.vue'
 </script>
 
 <style scoped>
---
+++ src/components/Btn.vue
@@ -0,0 +1,55 @@
+<template>
+  <button class="btn">
+    <span v-if="hasPrefixSlot" class="prefix">
+      <slot name="iconPrefix"/>
+    </span>
+    <span v-if="hasDefaultSlot" class="default">
+      <slot/>
+    </span>
+    <span v-else class="default">
+      Button
+    </span>
+    <span v-if="hasPostfixSlot" class="postfix">
+      <slot name="iconPostfix"/>
+    </span>
+  </button>
+</template>
+
+<script setup>
+import { useSlots } from 'vue';
+
+const slots = useSlots();
+const hasDefaultSlot = !!slots.default;
+const hasPrefixSlot = !!slots.iconPrefix;
+const hasPostfixSlot = !!slots.iconPostfix;
+</script>
+
+<style scoped>
+.btn {
+  border: none;
+  width: 100%;
+  height: 53px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  background: var(--grad);
+  color: var(--color-white);
+  transition: background .3s ease;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 100%;
+  letter-spacing: 0;
+
+  &:hover {
+    background: var(--grad-invert);
+    transition: background .3s ease;
+  }
+}
+.prefix {
+  width: 32px;
+  height: 32px;
+}
+</style>
