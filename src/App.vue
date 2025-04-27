f4594aaade9c3959d29aefffca9df7807be8cf4d
Rusich <vasenin@verqor.com>
2025-04-27 20:15:25 +0300

    added stat component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
+<script setup>
+import Btn from './components/Btn.vue'
+import Stat from './components/Stat.vue'
+import IconPoint from './icons/IconMapMarker.vue'
+
+const data = [
+    { label: 'Влажность', value: '90%' },
+    { label: 'Осадки', value: '0%' },
+    { label: 'Ветер', value: '3 м/ч' },
+];
+</script>
+
 <template>
-  <header>Header</header>
-  <main>
+  <main class="main">
     <Btn>
       <template #iconPrefix>
-        <img src="./assets/map-marker.svg" alt="icon"/>
+        <IconPoint/>
       </template>
       Изменить город
     </Btn>
+    <Stat v-for="(i, idx) in data" :key="idx" v-bind="i"/>
   </main>
-  <footer>Footer</footer>
 </template>
 
-<script setup>
-import Btn from './components/Btn.vue'
-</script>
-
 <style scoped>
-
+.main {
+  background-color: var(--color-main-bg);
+  width: 944px;
+  height: 644px;
+  border-radius: 26px;
+  padding: 50px;
+}
 </style>
---
+++ src/components/Stat.vue
@@ -0,0 +1,30 @@
+<script setup>
+// const { label, value } = defineProps(['label', 'value']);
+const { label = '---', value = '---' } = defineProps({
+  label: String,
+  value: String,
+});
+</script>
+
+<template>
+  <div class="stat">
+    <div class="label">{{label}}</div><div class="value">{{value}}</div>
+  </div>
+</template>
+
+<style scoped>
+.stat {
+  font-weight: 500;
+  font-size: 28px;
+  line-height: 100%;
+  letter-spacing: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 16px;
+}
+.label {
+  font-weight: 700;
+  text-transform: uppercase;
+}
+</style>
