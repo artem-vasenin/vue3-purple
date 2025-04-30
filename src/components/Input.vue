8db4f3937ac4c24ba3012ca0ab4d558c1f71183d
Rusich <vasenin@verqor.com>
2025-04-30 16:31:45 +0300

    added directive

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,9 +1,12 @@
 <script setup>
 const model = defineModel({ type: String });
+const vFocus = {
+  mounted: el =>el.focus(),
+}
 </script>
 
 <template>
-  <input v-model="model" type="text">
+  <input v-model="model" v-focus type="text">
 </template>
 
 <style scoped>
