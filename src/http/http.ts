2c725209a7fb830e80ef9647f946fd1686756725
Rusich <vasenin@verqor.com>
2025-05-01 23:15:35 +0300

    added axios

--- src/http/http.ts
+++ src/http/http.ts
@@ -1,12 +1,18 @@
-import {API_ROUTES} from "@/types/constants.ts";
+import axios from "axios";
+import {API_BASE, API_ROUTES} from "@/types/constants.ts";
 import type {IProfile} from "@/types/profile.ts";
 
 class Http {
+    ax = axios.create({
+        baseURL: API_BASE,
+        timeout: 10000,
+    });
+
     async getProfile(): Promise<IProfile | null> {
         try {
-            const res = await fetch(API_ROUTES.PROFILE);
-            if (!res) throw new Error('Request wrong');
-            return res.json();
+            const res = await this.ax.get<IProfile>(API_ROUTES.PROFILE);
+            if (!res.data) throw new Error('Request wrong');
+            return res.data;
         } catch (e) {
             console.error(e);
             return null;
--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,6 +1,6 @@
 export const API_BASE = 'http://localhost:3000/';
 export const API_ROUTES = {
-    CATEGORIES: `${API_BASE}categories`,
-    PROFILE: `${API_BASE}profile`,
-    BOOKMARKS: `${API_BASE}bookmarks`,
+    CATEGORIES: 'categories',
+    PROFILE: 'profile',
+    BOOKMARKS: 'bookmarks',
 };
